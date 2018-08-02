6a1ddc6f37155df26d3dca40a8eeb0474cf163b5
Dania801 <ssansnnom07@gmail.com>
2018-08-02 14:21:01 +0300

    Update transaction

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -20,6 +20,19 @@ describe('Transaction', () => {
           .toEqual(amount);
       });
 
+      it('should input the balance of the wallet', () => {
+          expect(transaction.input.amout).toEqual(wallet.balance);
+      });
+
+      it('validates a valid transaction', () => {
+        expect(Transaction.verifyTransaction(transaction)).toBe(true)
+      });
+
+      it('invalidates a corrupt transaction', () => {
+          transaction.outputs[0] = 5000000;
+        expect(Transaction.verifyTransaction(transaction)).toBe(false)
+      });
+
       describe('transacting with an amount that exceeds the balance', () => {
           beforeEach(() => {
               amount = 500000;
@@ -29,6 +42,25 @@ describe('Transaction', () => {
           it('does not create transaction', () => {
               expect(transaction).toEqual(undefined);
           })
-      })
+      });
+
+      describe('and updating a transaction', () => {
+        let nextAmount, nextRecipient;
+        beforeEach(() => {
+            nextAmount = 20;
+            nextRecipient = 'n3xt-4ddr355';
+            transaction = transaction.update(wallet, nextRecipient, nextAmount);
+        });
+
+        it(`subtracts the next amount from the sender's output`, () => {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+              .toEqual(wallet.balance - amount - nextAmount);
+          });
+      
+          it('outputs an amount for the next recipient', () => {
+            expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+              .toEqual(nextAmount);
+          });
+      });
 
 })
