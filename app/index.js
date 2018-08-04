56402020f4a3d638fa4467ad26995d63d13b4e9a
Dania801 <ssansnnom07@gmail.com>
2018-08-04 11:06:30 +0300

    Sync transaction pool between all peers

--- app/index.js
+++ app/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
+const Transaction = require('../wallet/transaction');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(bodyParser.json());
 
@@ -20,7 +25,23 @@ app.post('/mine', (req, res) => {
     console.log(`New Blog added ${block.toString()}`);
     p2pServer.syncChains();
     res.status(201).redirect('/blocks');
-})
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transactions', (req, res) => {
+    const transaction = wallet.createTransaction(req.body.recipient, req.body.amount, tp);
+    p2pServer.broadcastTransaction(transaction);
+    res.redirect('/transactions');
+});
+
+app.get('/public-key', (req, res) => {
+    res.json({
+        publicKey: wallet.publicKey
+    });
+});
 
 app.listen(HTTP_PORT, () => {
     console.log(`Listening in the port ${HTTP_PORT}`);
--- dev-test.js
+++ dev-test.js
@@ -6,10 +6,21 @@
 //     console.log(bc.addBlock(`foo ${i}`).toString());
 // }
 
-const Wallet = require('./wallet');
-const Transaction = require('./wallet/transaction');
+ const Wallet = require('./wallet');
+ const TransactionPool = require('./wallet/transaction-pool');
+// const Transaction = require('./wallet/transaction');
+// wallet = new Wallet();
+// amount = 50;
+// recipient = 'r3c1p13nt';
+// transaction = Transaction.newTransaction(wallet, recipient, amount);
+// console.log('The test!!!!',transaction.outputs.find(output => output.address === wallet.publicKey).amout);
+
 wallet = new Wallet();
-amount = 50;
-recipient = 'r3c1p13nt';
-transaction = Transaction.newTransaction(wallet, recipient, amount);
-console.log('The test!!!!',transaction.outputs.find(output => output.address === wallet.publicKey).amout);
+tp = new TransactionPool();
+let transaction, sendAmount, recipient;
+sendAmount = 50;
+recipient = 'r4nd0m-4ddr355';
+transaction = wallet.createTransaction(recipient, sendAmount, tp);
+wallet.createTransaction(recipient, sendAmount, tp);
+console.log('The transaction: ', transaction.outputs.find(output => output.address === wallet.publicKey).amount);
+//console.log(transaction.outputs.find(output => output.address === wallet.publicKey).amount);
